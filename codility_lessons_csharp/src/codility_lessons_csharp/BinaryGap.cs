b8fb5284cb39861c37e5866c73858d112a78f16a
cursoondasdechoque <ondasdechoque@cursopilatesbrasil.com.br>
2016-07-06 00:26:57 -0300

    upload project

---
+++ codility_lessons_csharp/src/codility_lessons_csharp/BinaryGap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codility_lessons_csharp
+{
+    public class BinaryGap
+    {
+		public int solution(int N)
+		{
+
+			string binary = Convert.ToString(N, 2);
+
+			int gapTemp = 0;
+			int gapSum = 0;
+
+			for (int i = 0; i <= binary.Length - 1; i++)
+			{
+				gapSum += binary[i] == '0' ? 1 : 0;
+
+				if (binary[i] == '1')
+				{
+					if (gapTemp < gapSum)
+						gapTemp = gapSum;
+					gapSum = 0;
+				}
+			}
+
+			return gapTemp;
+
+		}
+
+    }
+}
---
+++ codility_lessons_csharp/src/codility_lessons_csharp/CyclicRotation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codility_lessons_csharp
+{
+    public class CyclicRotation
+    {
+		public int[] solution(int[] A, int K)
+		{
+			int N = A.Length;
+
+			if (N == 0)
+				return A;
+
+			int[] B = new int[N];
+
+			if (K >= N)
+				K %= N;
+
+			if (K == 0)
+				return A;
+
+			for (int i = 0; i < N; i++)
+			{
+				B[i] = i < K ? A[N + i - K] : A[i - K];
+			}
+
+			return B;
+
+		}
+
+	}
+}

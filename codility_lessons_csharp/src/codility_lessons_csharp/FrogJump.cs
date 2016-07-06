612681fa57ae5fb7075b579db497d34d5ae23007
cursoondasdechoque <ondasdechoque@cursopilatesbrasil.com.br>
2016-07-06 00:36:16 -0300

    FrogJump and OddOccurrencesInArray

---
+++ codility_lessons_csharp/src/codility_lessons_csharp/FrogJump.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codility_lessons_csharp
+{
+    public class FrogJump
+    {
+		public int solution(int X, int Y, int D)
+		{
+			int distance = Y - X;
+			int calc = (int)((Y - X) / D);
+			return distance % D == 0 ? calc : calc + 1;
+		}
+
+	}
+}
---
+++ codility_lessons_csharp/src/codility_lessons_csharp/OddOccurrencesInArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codility_lessons_csharp
+{
+	public class OddOccurrencesInArray
+	{
+		public int solution(int[] A)
+		{
+			var unpaired = 0;
+
+			for (int i = 0; i < A.Length; i++)
+				unpaired ^= A[i];
+
+			return unpaired;
+
+		}
+	}
+}
